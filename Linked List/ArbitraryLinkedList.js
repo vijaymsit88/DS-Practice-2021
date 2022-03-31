39e7ccbabc94836049696942a57554bf8f53fdf1
Vijay Singh <vising@microsoft.com>
2022-03-31 09:47:28 +0530

    update

--- Linked List/ArbitraryLinkedList.js
+++ Linked List/ArbitraryLinkedList.js
@@ -1,45 +1,46 @@
 class Node {
-    constructor(value,next,arbit) {
-        this.next = next;
-        this.value = value;
-        this.arbit = arbit;
-    }
+  constructor(value, next, arbit) {
+    this.next = next;
+    this.value = value;
+    this.arbit = arbit;
+  }
 }
 
-
 class ArbitLinkedList {
-    constructor(value) {
-        this.head = new Node(value, null, null);
-        this.tail = this.head;
-        this.length = 1;
-    }
+  constructor(value) {
+    this.head = new Node(value, null, null);
+    this.tail = this.head;
+    this.length = 1;
+  }
 
-    append(value) {
-        const obj = new Node(value, null, null);
-        this.tail.next = obj;
-        this.tail = obj;
-        this.length++;
+  append(value) {
+    const obj = new Node(value, null, null);
+    this.tail.next = obj;
+    this.tail = obj;
+    this.length++;
+  }
+  printMyLinkedList() {
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      console.log(
+        `Node Value : ${currentNode.value}, next : ${currentNode?.next?.value}, arbit : ${currentNode.arbit.value}`
+      );
+      currentNode = currentNode.next;
     }
-    printMyLinkedList() {
-        let currentNode = this.head;
-        while(currentNode !== null) {
-            console.log(`Node Value : ${currentNode.value}, next : ${currentNode?.next?.value}, arbit : ${currentNode.arbit.value}` );
-            currentNode = currentNode.next;
-        }
-    }
-    setArbit(value, target) {
-        const currNode = this.iterateLinkedList(value);
-        const targetNode = this.iterateLinkedList(target);
-        currNode.arbit = targetNode;
-    }
+  }
+  setArbit(value, target) {
+    const currNode = this.iterateLinkedList(value);
+    const targetNode = this.iterateLinkedList(target);
+    currNode.arbit = targetNode;
+  }
 
-    iterateLinkedList(value) {
-        let currentNode = this.head;
-        while(currentNode.value !== value) {
-            currentNode = currentNode.next;
-        }
-        return currentNode;
+  iterateLinkedList(value) {
+    let currentNode = this.head;
+    while (currentNode.value !== value) {
+      currentNode = currentNode.next;
     }
+    return currentNode;
+  }
 }
 const myLinkedList = new ArbitLinkedList(1);
 myLinkedList.append(2);
@@ -58,34 +59,31 @@ myLinkedList.printMyLinkedList();
 copyLinkedList(myLinkedList);
 
 function copyLinkedList(myLinkedList) {
-    const copyLinkedList = new ArbitLinkedList(myLinkedList.head.value);
-    const map = {};
-    let currNodeOfCopyList = copyLinkedList.head;
-    let currNodeOfMainList = myLinkedList.head;
-    while(currNodeOfMainList!==null) {
-        if(currNodeOfMainList.next === null) {
-            currNodeOfCopyList.next = null;
-            copyLinkedList.tail = currNodeOfCopyList;
-        } else {
-            currNodeOfCopyList.next = new Node(currNodeOfMainList.next.value);
-        }
-        currNodeOfCopyList.arbit = new Node(currNodeOfMainList.arbit.value);
-        map[currNodeOfCopyList.value] =
-        {
-            arbit: currNodeOfCopyList.arbit,
-            next: currNodeOfCopyList.next,
-        }
-        currNodeOfCopyList = currNodeOfCopyList.next;
-        currNodeOfMainList = currNodeOfMainList.next;  
+  const copyLinkedList = new ArbitLinkedList(myLinkedList.head.value);
+  const map = {};
+  let currNodeOfCopyList = copyLinkedList.head;
+  let currNodeOfMainList = myLinkedList.head;
+  while (currNodeOfMainList !== null) {
+    if (currNodeOfMainList.next === null) {
+      currNodeOfCopyList.next = null;
+      copyLinkedList.tail = currNodeOfCopyList;
+    } else {
+      currNodeOfCopyList.next = new Node(currNodeOfMainList.next.value);
     }
+    currNodeOfCopyList.arbit = new Node(currNodeOfMainList.arbit.value);
+    // map[currNodeOfCopyList.value] = {
+    //   arbit: currNodeOfCopyList.arbit,
+    //   next: currNodeOfCopyList.next,
+    // };
+    currNodeOfCopyList = currNodeOfCopyList.next;
+    currNodeOfMainList = currNodeOfMainList.next;
+  }
 
-    let currNode = copyLinkedList.head;
-    while(currNode !== null) {
-        currNode.next = map[currNode.value].next;
-        currNode.arbit = map[currNode.value].arbit;
-        currNode = currNode.next;
-    }
-    copyLinkedList.printMyLinkedList();
+  //   let currNode = copyLinkedList.head;
+  //   while (currNode !== null) {
+  //     //currNode.next = map[currNode.value].next;
+  //     currNode.arbit = map[currNode.value].arbit;
+  //     currNode = currNode.next;
+  //   }
+  copyLinkedList.printMyLinkedList();
 }
-
-
